4a5efbce2b181eb49a53bc59fb4e2024173dda39
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2022-07-05 20:25:29 -0400

    updatin closest value

--- EasyQuestions/ClosestValueInBST.java
+++ EasyQuestions/ClosestValueInBST.java
@@ -1,23 +1,40 @@
 public class ClosestValueInBST {
-    int helperMethod(BinaryTreeNode tree,int target,int closest){
-        if(tree == null){
-            return closest;
+    // int helperMethod(BinaryTreeNode tree,int target,int closest){
+    //     if(tree == null){
+    //         return closest;
+    //     }
+    //     if(Math.abs(target - closest) > Math.abs(target - tree.value)){
+    //         closest = tree.value;
+    //     }
+    //     if(target < tree.value){
+    //         return helperMethod(tree.left,target,closest);
+    //     }
+    //     else if(target > tree.value){
+    //         return helperMethod(tree.right,target,closest);
+    //     }
+    //     else 
+    //         return closest;
+    // }
+
+    int helperMethod2(BinaryTreeNode tree,int target,int closest){
+        BinaryTreeNode temp = tree;
+        while( temp != null ){
+            if(Math.abs(target - closest) > Math.abs(target - temp.value)){
+                closest = temp.value;
+            }
+            if(target < temp.value){
+                temp = temp.left;
+            }
+            else if(target > tree.value){
+                temp = temp.right;
+            }
+            else break;
         }
-        if(Math.abs(target - closest) > Math.abs(target - tree.value)){
-            closest = tree.value;
-        }
-        if(target < tree.value){
-            return helperMethod(tree.left,target,closest);
-        }
-        else if(target > tree.value){
-            return helperMethod(tree.right,target,closest);
-        }
-        else 
-            return closest;
+        return closest;
     }
 
     int findClosestValue(BinaryTreeNode _tree, int _target){
-        return helperMethod(_tree,_target,Integer.MAX_VALUE);
+        return helperMethod2(_tree,_target,Integer.MAX_VALUE);
     }
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
@@ -30,10 +47,8 @@ public class ClosestValueInBST {
         tree.insert(13);
         tree.insert(22);
         tree.insert(14);
-        tree.printTree(tree.root);
-
-        ClosestValueInBST obj = new ClosestValueInBST();
-        System.out.print(obj.findClosestValue(tree.root, 12));
+        ClosestValueInBST object = new ClosestValueInBST();
+        System.out.print(object.findClosestValue(tree.root, 12));
 
     }
 }
